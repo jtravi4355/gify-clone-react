ce4168abf85adeb1358d6429db52b0441ee934f3
jtravi4355 <jrtravi43@gmail.com>
2020-07-23 23:38:53 -0400

    Fixed Title and built

---
+++ build/precache-manifest.a4ac7421a6c0728faa993976a4b3f5f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c91dab221d481708d93ef9b05afa2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c80553a195359ae91b51",
+    "url": "/static/css/main.289692ad.chunk.css"
+  },
+  {
+    "revision": "163a4f4920ceace1b132",
+    "url": "/static/js/2.7bc45c20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7bc45c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80553a195359ae91b51",
+    "url": "/static/js/main.2e76cafc.chunk.js"
+  },
+  {
+    "revision": "f1a48f09fd573378a1aa",
+    "url": "/static/js/runtime-main.b6635235.js"
+  }
+]);
